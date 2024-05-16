212cb1bb7a5f42b62e16bc775066f6cbac0468d5
Nageshwari Kiran <nageshwari.kiran@philips.com>
2024-05-16 00:19:32 -0700

    First set of changes

--- brightener.h
+++ brightener.h
@@ -3,6 +3,6 @@
 
 using namespace std;
 
-shared_ptr<Image> BrightenWholeImage(shared_ptr<Image> inputImage, int& attenuatedPixelCount);
-shared_ptr<Image> AddBrighteningImage(shared_ptr<Image> inputImage, shared_ptr<Image> imageToAdd,
+shared_ptr<Image> BrightenWholeImage(const shared_ptr<Image> inputImage, int& attenuatedPixelCount) ;
+shared_ptr<Image> AddBrighteningImage(const shared_ptr<Image> inputImage, const shared_ptr<Image> imageToAdd,
     int& attenuatedPixelCount);
